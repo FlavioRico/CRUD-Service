dd2684857c936060cbe457e5daa12cc2ce273438
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-08-25 17:50:50 -0500

    Estructura de rutas para lo componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
+import { EditarComponent } from './Persona/editar/editar.component';
+import { DeleteComponent } from './Persona/delete/delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'listar',
+    component: ListarComponent
+  },
+  {
+    path: 'add',
+    component: AgregarComponent
+  },
+  {
+    path: 'edit',
+    component: EditarComponent
+  },
+  {
+    path: 'eliminar',
+    component: DeleteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
