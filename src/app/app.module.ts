979f035f0871591267f6481f6a0c927c4b7c01c2
Flavio Rico <flavio.rico@fygdevelopers.com>
2020-08-25 17:52:43 -0500

    Modulos requeridos para el funcionamiento del proyecto e incorporación del CDN de Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
+import { EditarComponent } from './Persona/editar/editar.component';
+import { FormsModule } from '@angular/forms';
+import { ServiceService } from './Services/service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NotFoundPersonsComponent } from './not-found-persons/not-found-persons.component';
+import { LoaderComponent } from './loader/loader.component';
+import { DeleteComponent } from './Persona/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AgregarComponent,
+    EditarComponent,
+    NotFoundPersonsComponent,
+    LoaderComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ServiceService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
